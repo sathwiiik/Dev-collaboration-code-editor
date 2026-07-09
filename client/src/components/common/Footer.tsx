1bbb763c7abe4f77004f059696eb863d2a97c3a8
Sathwik Suman Dalabehera <134141400+sathwiiik@users.noreply.github.com>
2026-07-10 01:56:01 +0530

    Update footer link to new GitHub profile

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
             <span>
                 Build with ❤️ by{" "}
                 <a
-                    href="https://github.com/sahilatahar"
+                    href="https://github.com/sathwiiik"
                     className="text-primary underline underline-offset-1"
                 >
                     sahilatahar
